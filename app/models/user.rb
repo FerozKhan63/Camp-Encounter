108a60670e172a0c58dc50407a2c6ad3aa51b39b
Feroz Khan <feroz.khan@square63.org>
2021-11-03 17:45:25 +0500

    Fixed indetation and spacing issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,:confirmable
   
   PASSWORD_REGEX = /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/
-
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,:confirmable
 
   validates :first_name, :country, presence: true , length: { minimum: 2 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
   validates :phone_number, format: { with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/, message: "Phone numbers must be in xxx-xxx-xxxx format." }
