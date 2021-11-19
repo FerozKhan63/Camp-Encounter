7f9fd3b259d5ee34520f8acf38252ba82432c3a8
Feroz Khan <feroz.khan@square63.org>
2021-11-19 17:51:24 +0500

    removed esleif

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if current_user.admin? 
       users_path
-    elsif current_user.superadmin? 
-      root_path
-    elsif current_user.user? 
+    else
       root_path
     end
   end
