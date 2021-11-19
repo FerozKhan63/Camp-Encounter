e09e990af4675dcc7f3aa23dbaa51b299bd0f8e2
Feroz Khan <feroz.khan@square63.org>
2021-11-19 15:43:28 +0500

    Issues reloved in PR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   protected
-  def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name ,:email,:country,:country_code,:phone_number, :password,:terms_of_service])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name,:last_name, :email, :password,:country,:country_code,:phone_number, :current_password])
-      devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name, :phone_number, :country_code])
-  end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:invite, keys: [:first_name, :last_name, :role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name , :email, :country, :country_code, :phone_number, :password, :terms_of_service])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :country, :country_code, :phone_number, :current_password])
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name, :phone_number, :country_code])
   end
-  
+
   def after_sign_in_path_for(resource)
     if current_user.admin? 
       users_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,12 @@ class UsersController < AdminController
 
   def index
     if params[:query].present?
-      @pagy, @users = pagy(User.global_search(params[:query]).order(created_at: :asc), items: 3)
+      @users = User.global_search(params[:query]).order(created_at: :asc)
     else
-      @pagy, @users = pagy(User.order(created_at: :asc), items: 3)
+      @users = User.order(created_at: :asc)
     end
+    @pagy, @users = pagy(@users, items: 3)
+
     respond_to do |format|
       format.html
       format.csv { send_data @users.to_csv, filename: "users-#{Date.today}.csv" }
@@ -28,13 +30,12 @@ class UsersController < AdminController
     @user.skip_confirmation!
   
     raw, enc = Devise.token_generator.generate(User, :reset_password_token)
-    @user.reset_password_token   = enc
-    @user.reset_password_sent_at = Time.now.utc
+    @user.reset_password_token = enc
+    @user.reset_password_sent_at = Time.current
 
     if @user.save
       UserMailer.send_invite_email(@user,raw).deliver
-      redirect_to users_path
-      flash[:success] = "An invitation has been sent to the user!"
+      redirect_to users_path, notice: "An invitation has been sent to the user!"
     else
       flash.now[:error] = "There are some errors in the provided details. Please resubmit the form with the correct details."
       render :new
@@ -42,8 +43,6 @@ class UsersController < AdminController
   end
 
   def show
-    @user = User.find(params[:id])
-   
     respond_to do |format|
       format.js
       format.html 
